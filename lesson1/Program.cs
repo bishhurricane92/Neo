f7dff2fbb83a16c96b9ecef4c884af1bf59582fb
bishhurricane92 <bishhurricane92@gmail.com>
2020-01-21 16:42:24 +0600

    Добавьте файлы проекта.

---
+++ lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+	appBegin:
+		Console.WriteLine("==========================================================================");
+		Console.WriteLine("Что будет делать программа?");
+		Console.WriteLine("==========================================================================");
+		Console.WriteLine();
+
+		Console.WriteLine("Fuck the world\n");
+
+		string neobis = "Fuck the world";
+
+		Console.WriteLine($"{neobis}");
+
+
+		Console.WriteLine();
+		Console.WriteLine("==========================================================================");
+		Console.WriteLine("Выйти из программы? [y/n]:");
+		if (Console.ReadLine().Trim().ToLower() == "n") { goto appBegin; }
+	}
+}
